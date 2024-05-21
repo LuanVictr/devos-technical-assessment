4f60ddc0fe31962c505068e89322bb3b1258211c
Luan Victor <>
2024-05-21 17:26:20 -0300

    feat: updating regionService to create a region using token user information

--- app_api/src/services/regionService.ts
+++ app_api/src/services/regionService.ts
@@ -3,13 +3,15 @@ import { IRegion } from "../interfaces/IRegion";
 import { RegionModel } from "../models/models";
 
 export async function createNewRegion(newRegion: IRegion) {
-  if (!newRegion) {
+  if (!newRegion.name || !newRegion.region.coordinates) {
     throw {
       status: STATUS.BAD_REQUEST,
       message: "Region info is missing on request body",
     };
   }
+
+  console.log(newRegion)
   const createdRegion = await RegionModel.create(newRegion);
 
-  return { message: "Region created successfully", user: createdRegion };
+  return { message: "Region created successfully", newRegion: createdRegion };
 }
