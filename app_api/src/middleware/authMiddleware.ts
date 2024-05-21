edd3b79381b3ca5cfd8270ef814b15949d07ccaa
Luan Victor <>
2024-05-21 17:03:32 -0300

    feat: creating authMiddleware to validate token and send it to region controllers

---
+++ app_api/src/middleware/authMiddleware.ts
@@ -0,0 +1,19 @@
+import { validateToken } from "../utils/jwt";
+
+function authMiddleware(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  try {
+    const decoded = validateToken(token);
+    req.body.user = decoded;
+    next();
+  } catch (error: any) {
+    return res.status(403).json({ message: "Invalid token" });
+  }
+}
+
+export default authMiddleware;
