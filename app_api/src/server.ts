9547cd30989c92f0a351a98289f52854f136bebc
Luan Victor <>
2024-05-21 17:03:00 -0300

    feat: adding auth route to server.ts

--- app_api/src/server.ts
+++ app_api/src/server.ts
@@ -1,6 +1,7 @@
 import * as app from 'express';
 import userRoutes from './routes/userRoutes';
 import regionRoutes from './routes/regionRoutes';
+import authRoute from './routes/authRoute';
 import celebrateError from './infra/handlers/celebrateError';
 
 const server = app();
@@ -8,6 +9,7 @@ const server = app();
 server.use(app.json());
 server.use(userRoutes);
 server.use(regionRoutes);
+server.use(authRoute);
 server.use(celebrateError);
 
 export default server;
