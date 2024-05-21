2e552b0f5efdd8fda7ed5b6c43fcf9cc535195d6
Luan Victor <>
2024-05-21 16:25:14 -0300

    feat: adding region routes to server.ts

--- app_api/src/server.ts
+++ app_api/src/server.ts
@@ -1,12 +1,13 @@
 import * as app from 'express';
-import { UserModel } from './models/models';
 import userRoutes from './routes/userRoutes';
+import regionRoutes from './routes/regionRoutes';
 import celebrateError from './infra/handlers/celebrateError';
 
 const server = app();
 
 server.use(app.json());
 server.use(userRoutes);
+server.use(regionRoutes);
 server.use(celebrateError);
 
 export default server;
