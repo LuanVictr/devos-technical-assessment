e067bdf3eb33ec629b5777106e8302783a8d175a
Luan Victor <>
2024-05-23 16:16:00 -0300

    feat: creating get region by id controller function

--- app_api/src/controllers/regionController.ts
+++ app_api/src/controllers/regionController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { STATUS } from "../enums/status";
-import { createNewRegion } from "../services/regionService";
+import {
+  createNewRegion,
+  getRegionByIdService,
+} from "../services/regionService";
 
 const createRegion = async (req: Request, res: Response) => {
   const newRegion = req.body;
@@ -16,4 +19,18 @@ const createRegion = async (req: Request, res: Response) => {
   }
 };
 
-export { createRegion };
+const getRegionById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const regionFound = await getRegionByIdService(id);
+
+    return res.status(STATUS.OK).json(regionFound);
+  } catch (error) {
+    return res
+      .status(error.status ? error.status : STATUS.INTERNAL_SERVER_ERROR)
+      .json({ message: error.message });
+  }
+};
+
+export { createRegion, getRegionById };
