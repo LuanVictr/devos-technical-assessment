dace9ba493c7574fe041f98a2b4346e8e4dd9b4a
Luan Victor <>
2024-05-23 16:16:32 -0300

    feat: creating get region by id auth route

--- app_api/src/routes/regionRoutes.ts
+++ app_api/src/routes/regionRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createRegion } from "../controllers/regionController";
+import { createRegion, getRegionById } from "../controllers/regionController";
 import authMiddleware from "../middleware/authMiddleware";
 import regionValidation from "../validations/regionValidation";
 
 const router = Router();
 
+router.get("/region/:id", authMiddleware, getRegionById);
 router.post("/region", regionValidation.region , authMiddleware, createRegion );
 
 export default router;
