dbad474cd83d2f60c9ade02b8ba7d78ad79a7d92
Luan Victor <>
2024-05-24 15:29:09 -0300

    fix: changing update schema to simplify update regions routes body

--- app_api/src/validations/regionValidation.ts
+++ app_api/src/validations/regionValidation.ts
@@ -21,20 +21,17 @@ const regionSchema = {
 const regionUpdateSchema = {
   name: Joi.string(),
   user: Joi.string(),
-  region: Joi.object({
-    type: Joi.string().required(),
-    coordinates: Joi.array()
-      .items(
-        Joi.array()
-          .items(
-            Joi.array()
-              .items(Joi.number().required(), Joi.number().required())
-              .required()
-          )
-          .required()
-      )
-      .required(),
-  }),
+  coordinates: Joi.array()
+    .items(
+      Joi.array()
+        .items(
+          Joi.array()
+            .items(Joi.number().required(), Joi.number().required())
+            .required()
+        )
+        .required()
+    )
+    .required(),
 };
 
 const save = celebrate(
