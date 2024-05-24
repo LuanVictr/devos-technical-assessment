5313f5d94e880f6e7e3e5fc5b559468feb60c141
Luan Victor <>
2024-05-24 14:08:44 -0300

    feat: creating delete region auth route

--- app_api/src/routes/regionRoutes.ts
+++ app_api/src/routes/regionRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createRegion, getRegionById, updateRegionById } from "../controllers/regionController";
+import { createRegion, deleteRegion, getRegionById, updateRegionById } from "../controllers/regionController";
 import authMiddleware from "../middleware/authMiddleware";
 import regionValidation from "../validations/regionValidation";
 
@@ -8,5 +8,6 @@ const router = Router();
 router.get("/region/:id", authMiddleware, getRegionById);
 router.post("/region", regionValidation.save , authMiddleware, createRegion );
 router.put("/region/:id", regionValidation.update, authMiddleware, updateRegionById);
+router.delete("/region/:id", authMiddleware, deleteRegion);
 
 export default router;
