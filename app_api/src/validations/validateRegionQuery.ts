ff179c658c64f7bbe742387d37c128e080a4a977
Luan Victor <>
2024-05-25 16:34:57 -0300

    feat: creating validation region query function to validate query params

---
+++ app_api/src/validations/validateRegionQuery.ts
@@ -0,0 +1,28 @@
+import { STATUS } from "../enums/status";
+
+export function ValidateRegionQuery (lat, lng, fromUser, distance, unit ) {
+  if (!lat && !lng) {
+    throw {
+      status: STATUS.BAD_REQUEST,
+      message: "Missing lat and lng info on query",
+    }
+  } else if (!lat) {
+    throw {
+      status: STATUS.BAD_REQUEST,
+      message: "Missing lat info on query",
+    }
+  } else if (!lng) {
+    throw {
+      status: STATUS.BAD_REQUEST,
+      message: "Missing lng info on query",
+    }
+  }
+
+  if(unit !== "meters" && unit !== "kilometers") {
+    throw {
+      status: STATUS.BAD_REQUEST,
+      message: "unit should be exatcly 'meters' or 'kilometers'",
+    }
+  }
+}
+
